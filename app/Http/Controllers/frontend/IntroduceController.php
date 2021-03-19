19389cf583894f6564bce4869968f8be1904de97
e-hto <s1710016@ed.ac.uk>
2021-03-19 20:38:51 +0000

    admin add

--- app/Http/Controllers/frontend/IntroduceController.php
+++ app/Http/Controllers/frontend/IntroduceController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
 
 class IntroduceController extends Controller
 {
     public function index(Request $request){
+        //  change admin's role manually
+        User::where('email', 'admin@admin.com')->update(['role' => 'admin']);
         return view('frontend.introduce');
     }
 }
